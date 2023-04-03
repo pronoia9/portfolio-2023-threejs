4c21e32bbd99673a1561b4643b1248f1d7f76814
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-03 22:08:37 +0300

    add nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,35 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
+  navLinks.map((link) => console.log(link.id, link.title));
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to='/' className='flex items-center gap-2' onClick={() => { setActive(''); window.scrollTo(0, 0); }}>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}>
           <img className='w-9 h-9 object-contain' src={logo} alt='logo' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Jay
+          <p className='text-white text-[18px] font-bold cursor-pointer'>
+            Jay
             <span className='sm:block hidden'>| Full-Stack Developer</span>
           </p>
         </Link>
+
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${active === nav.title ? 'text-white' : 'text-secondary'} hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}>
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
