3ce6780a51d5085f473d0e5ad2c3fc490d86c3ae
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-03 23:06:49 +0300

    add intro

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,16 @@ const Hero = () => {
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Jay</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
+          </p>
+        </div>
       </div>
     </section>
   );
