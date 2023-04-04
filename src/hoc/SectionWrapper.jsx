734aaa63dedf68e193b79b88426bfe96de27c14a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-05 00:15:11 +0300

    add initial wrapper

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,10 @@
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { staggerContainer } from '../utils/motion';
+
+const SectionWrapper = (Component, idName) => {
+  return <div>SectionWrapper</div>;
+};
+
+export default SectionWrapper;
