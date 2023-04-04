70797f87507558df4b7ade467cbc6c295bc36b60
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-05 01:00:51 +0300

    finish timeline cards

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,32 @@ import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
 
-const ExperienceCard = ({ experience }) => <></>;
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: '#1d1836', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  #232631' }}
+    date={experience.date}
+    iconStyle={{ background: experience.iconBg }}
+    icon={
+      <div className='flex justify-center items-center w-full h-full'>
+        <img src={experience.icon} alt={experience.company_name} className='w-[60%] h-[60%] object-contain' />
+      </div>
+    }>
+    <div>
+      <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+      <p className='text-secondary text-[16px] font-semibold' style={{ margin: 0 }}>
+        {experience.company_name}
+      </p>
+    </div>
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point, index) => (
+        <li className='text-white-100 text-[14px] pl-1 tracking-wider' key={index}>
+          {point}
+        </li>
+      ))}
+    </ul>
+  </VerticalTimelineElement>
+);
 
 const Experience = () => {
   return (
@@ -19,7 +44,9 @@ const Experience = () => {
 
       <div className='mt-2 flex flex-col'>
         <VerticalTimeline>
-          {experiences.map((experience, idx) => (<ExperienceCard key={idx} experience={experience} />))}
+          {experiences.map((experience, idx) => (
+            <ExperienceCard key={idx} experience={experience} />
+          ))}
         </VerticalTimeline>
       </div>
     </>
