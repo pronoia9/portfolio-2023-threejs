40023c61871c70df6a6c7743882b41062b0934e6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-04 23:59:15 +0300

    add styles to subtext

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-      <motion.p variants={fadeIn('', '', 0.1, 1)}>
+      <motion.p className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]' variants={fadeIn('', '', 0.1, 1)}>
         I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like
         React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable,
         and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
