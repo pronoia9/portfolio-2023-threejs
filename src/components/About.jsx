80df425b5b9e5691ecc13f9b333233503b0eff22
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-05 00:23:36 +0300

    format

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Tilt } from 'react-tilt';
-
 import { motion } from 'framer-motion';
+
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
