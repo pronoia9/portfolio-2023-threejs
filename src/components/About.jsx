c7c719a96bc694423dfc3f6bf6e2c5aca9a30a58
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-04 23:55:44 +0300

    fix import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
-import { fadeIn, textVarian } from '../utils';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
