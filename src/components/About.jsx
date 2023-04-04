fd991fd5933ae2656f7036688985ceba8c52a9d8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-05 00:04:44 +0300

    add initial cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,8 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
+const ServiceCard = () => <></>;
+
 const About = () => {
   return (
     <>
@@ -13,11 +15,18 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
       <motion.p className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]' variants={fadeIn('', '', 0.1, 1)}>
         I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like
         React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable,
         and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
