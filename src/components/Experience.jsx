3866c8105b364c9b70f4869b1cbe0a3d6b6bc0b4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-05 00:55:06 +0300

    add title + init exp card

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,12 +7,22 @@ import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
 
+const ExperienceCard = ({ experience }) => <></>;
+
 const Experience = () => {
   return (
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>What I Have Done So Far</p>
-      <h2 className={styles.sectionHeadText}>Work Experience.</h2>
-    </motion.div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I Have Done So Far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+
+      <div className='mt-2 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience, idx) => (<ExperienceCard key={idx} experience={experience} />))}
+        </VerticalTimeline>
+      </div>
+    </>
   );
 };
 
