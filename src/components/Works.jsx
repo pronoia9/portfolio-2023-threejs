eecb98179adba7c18eef689b371a70246603434f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-06 20:42:12 +0300

    add styles to name/desc

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -22,8 +22,8 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
       </div>
 
       <div className='mt-5'>
-        <h3>{name}</h3>
-        <p>{description}</p>
+        <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
       </div>
     </Tilt>
   </motion.div>
