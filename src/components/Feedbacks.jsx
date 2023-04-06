73f708f369e892c513cfb0d247776f95aea00d00
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-06 22:19:41 +0300

    finish testimonial card

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -10,7 +10,19 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
     className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
     variants={fadeIn('', 'spring', index * 0.5, 0.75)}>
     <p className='text-white font-black text-[48px]'>"</p>
-    <p>{testimonial}</p>
+
+    <div className='mt-1'>
+      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+
+      <div className='mt-7 flex justify-between items-center gap-1'>
+        <div className='flex-1 flex flex-col'>
+          <p className='text-white font-medium text-[16px]'><span className='blue-text-gradient'>@</span> {name}</p>
+          <p className='mt-1 text-secondary text-[12px]'>{designation} of {company}</p>
+        </div>
+
+        <img className='w-10 h-10 rounded-full object-cover' src={image} alt={`feedback_by-${name}`} />
+      </div>
+    </div>
   </motion.div>
 );
 
