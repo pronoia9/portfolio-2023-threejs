b27d983758e18a9b3cb23c7e6ded75a2835a9fbb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-06 20:19:57 +0300

    send props

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -27,7 +27,7 @@ const Works = () => {
 
       <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`}></ProjectCard>
+          <ProjectCard key={`project-${index}`} index={index} {...project}></ProjectCard>
         ))}
       </div>
     </>
