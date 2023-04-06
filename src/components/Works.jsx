6a9d439df05bdb49cb8cdde71aac03874aa9eda9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-06 20:17:13 +0300

    add intro

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,6 +14,14 @@ const Works = () => {
         <p className={styles.sectionSubText}>Some of my work</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]' variants={fadeIn('', '', 0.1, 1)}>
+          Following projects showcases my skills and experience through real-world examples of my work. Each project is
+          briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex
+          problems, work with different technologies, and manage projects effectively.
+        </motion.p>
+      </div>
     </>
   );
 };
