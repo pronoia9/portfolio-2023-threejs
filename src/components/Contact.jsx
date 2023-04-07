d44a9bcbc3a424ea4cbe2bf2a3377f5abcae3fb1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-07 20:34:07 +0300

    add imports

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjS from '@emailjs/browser';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  return <div>Contact</div>;
+};
 
-export default Contact
+export default Contact;
