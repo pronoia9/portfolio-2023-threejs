2d559b711d6d49d3a3a79d02f586c0e2f06abdb6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-07 20:43:18 +0300

    add title + subtitle

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,10 +12,17 @@ const Contact = () => {
   const [form, setForm] = useState({ name: '', email: '', message: '' });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => { }
-  const handleSubmit = (e) => {}
+  const handleChange = (e) => {};
+  const handleSubmit = (e) => {};
 
-  return <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'></div>;
+  return (
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div className='flex-[0.75] bg-black-100 p-8 rounded-xl' variants={slideIn('left', 'tween', 0.2, 1)}>
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
+  );
 };
 
 export default SectionWrapper(Contact, 'contact');
