c7b66618919589076db9e79175fac2cc50faa6f4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-07 22:40:05 +0300

    add scroll state + format

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,17 @@ import { logo, menu, close } from '../assets';
 const Navbar = () => {
   const [active, setActive] = useState('');
   const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) setScrolled(true);
+      else setScrolled(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   const getLinks = (ulStyle, liStyle, liOnClick) => (
     <ul className={ulStyle}>
@@ -26,7 +37,10 @@ const Navbar = () => {
   );
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? 'bg-primary' : 'bg-transparent'
+      }`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to='/'
@@ -41,10 +55,7 @@ const Navbar = () => {
           </p>
         </Link>
         {/* Navbar */}
-        {getLinks(
-          'list-none hidden sm:flex flex-row gap-10',
-          'hover:text-white text-[18px] font-medium cursor-pointer'
-        )}
+        {getLinks('list-none hidden sm:flex flex-row gap-10', 'hover:text-white text-[18px] font-medium cursor-pointer')}
         {/* Mobile menu */}
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img
@@ -53,7 +64,10 @@ const Navbar = () => {
             alt='menu'
             onClick={() => setToggle(!toggle)}
           />
-          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+          <div
+            className={`${
+              !toggle ? 'hidden' : 'flex'
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
             {getLinks(
               'list-none flex justify-end items-start flex-1 flex-col gap-4',
               'font-poppins font-medium cursor-pointer text-[16px]',
