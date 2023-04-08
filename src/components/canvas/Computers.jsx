47becb59c851c77d276f8c376f32d89811fa1a7c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-08 05:08:02 +0300

    edit settings for the new scene

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,18 +5,15 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+  const computer = useGLTF('./room/scene.gltf');
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
-      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 0.9}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, 0]}
+        rotation={[0, 0.185, 0]}
       />
     </mesh>
   );
@@ -48,7 +45,22 @@ const ComputersCanvas = () => {
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}>
+      gl={{ preserveDrawingBuffer: true }}
+      onCreated={({ camera, gl, scene }) => {
+        // gl.setPixelRatio(window.devicePixelRatio);
+        // gl.outputEncoding = sRGBEncoding;
+        gl.physicallyCorrectLights = true;
+        gl.shadowMap.enabled = true;
+        // gl.shadowMap.type = PCFSoftShadowMap;
+        // gl.toneMapping = ACESFilmicToneMapping;
+        // console.log({ camera });
+        // const yAxis = new Vector3(0, 1, 0);
+        // const xAxis = new Vector3(1, 0, 0);
+        // camera.setRotationFromAxisAngle(yAxis, 60);
+        // camera.lookAt([0, -50, 0]);
+        // // camera.rotateY(Math.PI);
+        // camera.updateProjectionMatrix();
+      }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Computers isMobile={isMobile} />
